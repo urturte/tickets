06f3e499e638e7aa31fe80d98b5240c28a160c3c
Urtė Nistirenko <urte.nistirenko@gmail.com>
2022-12-21 19:08:45 +0200

    updated buytickets

--- api/controllers/tickets.js
+++ api/controllers/tickets.js
@@ -22,26 +22,12 @@ module.exports.CREATE_TICKET = (req, res) => {
   });
 };
 module.exports.BUY_TICKET = async (req, res) => {
-  const ticket = await TicketSchema.findOne({ _id: req.params.id })
-    .then((result) => {
-      console.log(result);
-      return result;
-    })
-    .then((ticket) => {
-      return ticket;
-    });
-  const fund = await UserSchema.findOne({ _id: req.body.id })
-    .then((result) => {
-      console.log(result.moneyBalance);
-      return result.moneyBalance;
-    })
-    .then((moneyBalance) => {
-      return moneyBalance;
-    });
+  const ticket = await TicketSchema.findOne({ _id: req.params.id });
+  const fund = await UserSchema.findOne({ _id: req.body.id }).then((result) => {
+    console.log(result.moneyBalance);
+    return result.moneyBalance;
+  });
   const balanceAfterPurchase = fund - ticket.ticketPrice;
-  console.log(ticket.ticketPrice);
-  console.log(fund);
-  console.log(ticket.ticketPrice < fund);
   if (ticket.ticketPrice < fund) {
     UserSchema.updateOne(
       { _id: req.body.id },
